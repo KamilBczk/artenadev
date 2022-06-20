047035642107773a49f8cf2327735493a234d517
KamilBczk <kamil.biczyk.pro@gmail.com>
2022-06-20 02:17:58 +0200

    header update

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,4 +1,37 @@
 $(document).ready(function () {
+    
+    // Scroll
+    $(window).scroll(function () {
+        var scroll = $(this).scrollTop();
+        var oldscroll = $(".scroll-info").val();
+        // console.log(scroll + " " + oldscroll);
+
+        if (scroll > oldscroll) {
+            console.log("bottom");
+        }
+        else {
+            console.log("top");
+        }
+
+        if (scroll >= 100) {
+            if (scroll > oldscroll) {
+                $(".header").addClass("header-hide");
+                $(".header").removeClass("header-show");
+            } else {
+                $(".header").addClass("header-show");
+                $(".header").removeClass("header-hide");
+            }
+            $(".header").addClass("header-minified");
+        } else {
+                $(".header").removeClass("header-show");
+                $(".header").removeClass("header-hide");
+                $(".header").removeClass("header-minified");
+        }
+        $(".scroll-info").val(scroll);
+    })
+   
+
+    // Theme
     $(".header-theme svg").click(function () {
         $(this).toggleClass("light");
     });
