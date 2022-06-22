2217ce61c2728b0d22b9547fdbbaac09f0cbaa02
unknown <kamil.biczyk.pro@gmail.com>
2022-06-23 00:06:01 +0200

    js update

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,16 +1,27 @@
-var tween = new TimelineMax()
-.add(TweenMax.to("#addTranslate", 1, {className: "+=animated"}),"first")
-.add(TweenMax.to("#addLetterSpacing", 1, {opacity: "0"}),"first")
-
 var controller = new ScrollMagic.Controller();
 
-// var animateElem = document.getElementById("picture");
+var firstTween = new TimelineMax()
+.add(TweenMax.to("#addTranslate", 1, {className: "+=animated"}),"first")
+.add(TweenMax.to("#addLetterSpacing", 1, {opacity: "0"}),"first")
 new ScrollMagic.Scene({
     triggerElement: "#startTextAnimation",
     duration: 1000,
     triggerHook: 0
 })
-.setTween(tween)
+.setTween(firstTween)
 .setPin("#setPin")
-// .addIndicators()
+.addIndicators()
 .addTo(controller);
+
+var secondTween = new TimelineMax()
+.add(TweenMax.to("#animTranslate", 1, {left: "70%"}), "first")
+// .add(TweenMax.to("#animTranslate", 1, {transform: "rotate(15deg)"}), "first");
+
+new ScrollMagic.Scene({
+    triggerElement: "#startPhoneAnimation",
+    duration: 500,
+})
+.setTween(secondTween)
+.addIndicators()
+.addTo(controller);
+
