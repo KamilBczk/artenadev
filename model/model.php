4dbeb5eecccd1ff86dae62222d19a45f7b755a9c
unknown <kamil.biczyk.pro@gmail.com>
2022-06-23 03:14:16 +0200

    model & vue update

--- model/model.php
+++ model/model.php
@@ -18,7 +18,7 @@ $url = explode('/', $_GET['url']);
 <html lang="en">
 <head>
     <!-- META -->
-    <title>Artena Dev_ - Home</title>
+    <title>Artena Dev_  |  Home</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,6 +27,7 @@ $url = explode('/', $_GET['url']);
     <link rel="stylesheet" href="/assets/css/reset.css">
     <link rel="stylesheet" href="/assets/css/style.css">
     <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css"> <!-- AOS -->
+    <link rel="stylesheet" href="http://textillate.js.org/assets/animate.css">
     <!-- JS DEFER -->
     <script defer src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script defer src="https://unpkg.com/aos@next/dist/aos.js"></script>
@@ -36,6 +37,8 @@ $url = explode('/', $_GET['url']);
     <script defer src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/debug.addIndicators.min.js"></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
     <script defer src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.7/plugins/animation.gsap.min.js"></script>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/lettering.js/0.7.0/jquery.lettering.min.js"></script>
+    <script defer src="https://cdnjs.cloudflare.com/ajax/libs/textillate/0.4.0/jquery.textillate.min.js"></script>
     <script defer src="/assets/js/header.js"></script>
     <script defer src="/assets/js/loader.js"></script>
     <script defer src="/assets/js/setup.js"></script>
@@ -47,7 +50,7 @@ $url = explode('/', $_GET['url']);
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
 </head>
-<body class="loader">
+<body class="loader-active">
     <?php include_once "model/assets/loader.html"; ?>
     <?php include_once "model/assets/header.html"; ?>
     <?php include_once "vue/$url[0].html"; ?>
